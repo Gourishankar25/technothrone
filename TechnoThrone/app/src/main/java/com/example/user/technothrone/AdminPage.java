074e14e64c3f7d9aaa577ac38bcf966d3ff334d0
Aghamarsh <34450480+Aghamarsh@users.noreply.github.com>
2019-02-26 13:36:30 +0530

    update

--- TechnoThrone/app/src/main/java/com/example/user/technothrone/AdminPage.java
+++ TechnoThrone/app/src/main/java/com/example/user/technothrone/AdminPage.java
@@ -17,23 +17,23 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
+import static java.lang.Math.abs;
+
 public class AdminPage extends AppCompatActivity {
 
     DatabaseReference displayQuestion;
     EditText qno;
     DatabaseReference pot;
-    int i;
-    //long amnt[]=new long[10];
-    ArrayList<String> amnt = new ArrayList<>();
-
+    DatabaseReference winner;
+    long c;
+   // ArrayList<String> amnt = new ArrayList<>();
+    long tot;
+    String maxi = "";
+     long max = -9999;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
-
-
-
-
     }
 
     public void pushQuestion(View v)
@@ -53,7 +53,17 @@ public class AdminPage extends AppCompatActivity {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 if(!dataSnapshot.getKey().equals("Total"))
-                    amnt.add(dataSnapshot.getValue().toString());
+                {
+                    long t = Integer.valueOf(dataSnapshot.getValue().toString());
+                    if(max<t)
+                    {
+                        max = t;
+                        maxi = dataSnapshot.getKey();
+                    }
+                    tot+=abs(Integer.valueOf(dataSnapshot.getValue().toString()));
+                    pot.child("Total").setValue(tot);
+                }
+                //amnt.add(dataSnapshot.getValue().toString());
             }
 
             @Override
@@ -76,8 +86,11 @@ public class AdminPage extends AppCompatActivity {
 
             }
         });
+        tot=0;
 
-        long total =0;
+
+
+       /* long total =0;
         long max = 0;
 
         for(int i=0;i<amnt.size();i++)
@@ -88,7 +101,47 @@ public class AdminPage extends AppCompatActivity {
                 max = i;
             }
             total+= a;
+        } */
+
+
+    }
+    public void addamount(View view)
+    {
+        if(!maxi.equals(""))
+        {
+            winner=FirebaseDatabase.getInstance().getReference().child("user").child(maxi).child("balance");
+            winner.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    String a=dataSnapshot.getValue().toString();
+                    c=Integer.valueOf(a);
+                    c+=tot;
+                    winner.setValue(c);
+
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
+        maxi="";
     }
 
 
